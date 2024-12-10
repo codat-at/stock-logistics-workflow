67d1bb35aab18254babb4e3749bf77c7cfb25a56
Philipp Ritzberger <philipp@codat.at>
2024-12-10 15:19:41 +0100

    [FIX] stock_picking_product_availability_search: fix the manifest version

--- stock_picking_product_availability_search/__manifest__.py
+++ stock_picking_product_availability_search/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Picking Product Availability Search",
     "summary": "Filter pickings by their products availability state",
-    "version": "17.0",
+    "version": "17.0.1.0",
     "author": "Camptocamp, Odoo Community Association (OCA),",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/stock-logistics-workflow",
